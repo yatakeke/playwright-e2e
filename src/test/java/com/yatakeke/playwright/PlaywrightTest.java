b3aee1dd7decbe289a636bed5af073306af0e8aa
yatakeke <yatasunshine.1456@gmail.com>
2024-03-16 13:53:01 +0900

    setup playwright test environment

---
+++ src/test/java/com/yatakeke/playwright/PlaywrightTest.java
@@ -0,0 +1,23 @@
+package com.yatakeke.playwright;
+
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class PlaywrightTest {
+
+    @Test
+    public void trialTest() {
+        try (Playwright playwright = Playwright.create()) {
+            var browser = playwright.chromium().launch();
+            var page = browser.newPage();
+            page.navigate("http://playwright.dev");
+
+            assertThat(page).hasTitle(Pattern.compile("Playwright"));
+
+        }
+    }
+}
