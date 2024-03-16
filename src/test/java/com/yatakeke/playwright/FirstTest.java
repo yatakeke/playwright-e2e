baf1ccfc9f5ebb5ffc02f66dbb8c4ccad77a9616
yatakeke <yatasunshine.1456@gmail.com>
2024-03-16 10:42:17 +0900

    first test passed

---
+++ src/test/java/com/yatakeke/playwright/FirstTest.java
@@ -0,0 +1,12 @@
+package com.yatakeke.playwright;
+
+
+import org.junit.jupiter.api.Test;
+import static com.google.common.truth.Truth.assertThat;
+
+public class FirstTest {
+    @Test
+    public void firstTest() {
+        assertThat(1).isEqualTo(1);
+    }
+}
