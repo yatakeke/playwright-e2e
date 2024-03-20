04218d81610bc996410aa488659ef2951adfc935
yatakeke <yatasunshine.1456@gmail.com>
2024-03-20 12:53:47 +0900

    first failed test

--- src/e2eTest/java/AnimalListPageTest.java
+++ src/e2eTest/java/AnimalListPageTest.java
@@ -1,11 +1,45 @@
-import org.junit.jupiter.api.Test;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.*;
 
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnimalListPageTest {
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    Page page;
 
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
     @Test
-    public void firstTestOnE2EModule() {
-        assertEquals(1, 0);
+    public void showAllAnimals() {
+        page.navigate("http://localhost:8080/animals");
+        var firstAnimalId = page.locator("id=animal-id");
+        var firstAnimalName = page.locator("id=animal-name");
+        assertThat(firstAnimalId).containsText("1");
+        assertThat(firstAnimalName).containsText("dog");
     }
 }
