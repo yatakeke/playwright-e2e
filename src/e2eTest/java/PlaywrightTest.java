5ff47a8fc06d9833d568b2123612b75b2cf43efb
yatakeke <yatasunshine.1456@gmail.com>
2024-03-20 12:34:42 +0900

    passed tutorial test

--- src/e2eTest/java/PlaywrightTest.java
+++ src/e2eTest/java/PlaywrightTest.java
@@ -1,21 +1,49 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.regex.Pattern;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlaywrightTest {
 
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+
     @Test
     public void trialTest() {
-        try (Playwright playwright = Playwright.create()) {
-            var browser = playwright.chromium().launch();
-            var page = browser.newPage();
-            page.navigate("https://playwright.dev");
 
-            assertThat(page).hasTitle(Pattern.compile("Playwright"));
-
-        }
+        page.setContent("<input id='checkbox' type='checkbox'></input>");
+        page.locator("input").check();
+        assertTrue((Boolean) page.evaluate("() => window['checkbox'].checked"));
     }
 }
